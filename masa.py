56a3fb2dbaf0561a1057922260408b537c78a1e1
Adalid <adalidorellanacut@gmail.com>
2023-09-13 10:14:30 -0300

    subiendo archivo importados, Anibal Paez 4°A

---
+++ masa.py
@@ -0,0 +1,10 @@
+def gramos_a_kilogramos(grammos):
+    return gramos/ 1000
+
+    def toneladas_a_kilogramos(kilogramos):
+        return toneladas *1000
+
+if __name__=="__main__":
+    gramos= 2.5
+    kilogramos= gramos_a_kilogramos(gramos)
+    print(f"(gramos) gr son equivalentes a (kilogramos) kg")
